e0103c67d45c9b3f197239a326530bdfea3d3495
CShah44 <chaityashah36@gmail.com>
2021-05-29 20:18:47 +0530

    Changed Header

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { signOut, useSession } from "next-auth/client";
 import Image from "next/image";
 import Login from "./Login";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 
 function Header() {
   const [session] = useSession();
@@ -14,20 +14,18 @@ function Header() {
   return (
     <Navbar bg="light" variant="light" sticky="top" expand="lg">
       <Container>
-        <Navbar.Brand>Global Social</Navbar.Brand>
-        <Nav className="d-flex justify-content-center align-items-center">
-          <Nav.Link>
+        <Navbar.Brand className="fs-2">Global Social</Navbar.Brand>
+        <Nav className="d-flex justify-content-center align-items-center fs-5">
+          <Nav.Link className="noUnderLine px-2">
             <Link href="/">Feed</Link>
           </Nav.Link>
-          <Nav.Link>
+          <Nav.Link className="noUnderLine px-2">
             <Link href="/chat">Chat</Link>
           </Nav.Link>
         </Nav>
         <Nav className="d-flex justify-content-center align-items-center">
-          <Nav.Item>
+          <Nav.Item style={{ cursor: "pointer" }}>
             <Image
-              onClick={signOut}
-              style={{ cursor: "pointer" }}
               src={session.user.image}
               className="rounded-full"
               width={40}
@@ -35,7 +33,12 @@ function Header() {
               layout="fixed"
             />
           </Nav.Item>
-          <Nav.Item className="mx-1">{session.user.name}</Nav.Item>
+          <Nav.Item className="mx-3 p-1">{session.user.name}</Nav.Item>
+          <Nav.Item>
+            <Button className="mx-2" variant="outline-dark" onClick={signOut}>
+              Sign Out
+            </Button>
+          </Nav.Item>
         </Nav>
       </Container>
     </Navbar>

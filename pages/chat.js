e573801b66156bca41fd3926ac38bff2f307da32
CShah44 <chaityashah36@gmail.com>
2021-06-03 11:02:11 +0530

    Deleted Old Chat functionality

--- pages/chat.js
+++ pages/chat.js
@@ -1,32 +1,5 @@
-import { Fragment } from "react";
-import Sidebar from "../components/Chat/Sidebar";
-import Header from "../components/Header";
-import { ContactsProvider } from "../contexts/ContactsProvider";
-import {
-  ConversationsProvider,
-  useConversations,
-} from "../contexts/ConversationsProvider";
-import { SocketProvider } from "../contexts/SocketProvider";
-import OpenConversation from "../components/Chat/OpenConversation";
-
 function chat() {
-  // const { selectedConversation } = useConversations();
-
-  return (
-    <Fragment>
-      <Header />
-      <ContactsProvider>
-        <ConversationsProvider>
-          <SocketProvider>
-            <div className="d-flex" style={{ height: "100vh" }}>
-              <Sidebar />
-              {/* {selectedConversation && <OpenConversation />} */}
-            </div>
-          </SocketProvider>
-        </ConversationsProvider>
-      </ContactsProvider>
-    </Fragment>
-  );
+  return <div>Hello Chat World!</div>;
 }
 
 export default chat;

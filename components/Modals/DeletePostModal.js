ed12675dd2902dbd4b31be7eb657b4fc23265e5e
CShah44 <chaityashah36@gmail.com>
2021-06-26 20:35:29 +0530

    Added a feedback Modal for deleting post

---
+++ components/Modals/DeletePostModal.js
@@ -0,0 +1,22 @@
+import { Modal, Button } from "react-bootstrap";
+
+function DeletePostModal({ show, hideModal, deletePost }) {
+  return (
+    <Modal show={show} onHide={hideModal} backdrop="static" keyboard={false}>
+      <Modal.Header>
+        <Modal.Title>Delete Post</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this post?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={hideModal}>
+          Cancel
+        </Button>
+        <Button variant="outline-danger" onClick={deletePost}>
+          Yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default DeletePostModal;

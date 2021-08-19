3b536c9b60bc64d05cb17f26801040d28efc19c0
CShah44 <chaityashah36@gmail.com>
2021-08-19 19:22:48 +0530

    Designing Login Component

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,6 @@
 import { Button } from "react-bootstrap";
 import { auth, provider } from "../firebase";
+import Image from "next/image";
 
 function Login() {
   function signIn() {
@@ -7,10 +8,29 @@ function Login() {
   }
 
   return (
-    <div className="d-grid place-items-center">
-      <Button onClick={signIn} variant="dark">
-        Login with Google
-      </Button>
+    <div
+      className="d-flex flex-column"
+      style={{ height: "100vh", backgroundColor: "black" }}
+    >
+      <div className="mx-auto mt-4">
+        <Image
+          src="/Logo.png"
+          width="500"
+          className="border border-white"
+          height="500"
+        />
+      </div>
+      <div className="mx-auto m-2">
+        <Button onClick={signIn} className="mx-1" variant="light">
+          Login with Google
+        </Button>
+        <Button className="mx-2" variant="light">
+          Login with Twitter
+        </Button>
+        <Button variant="light" className="mx-1">
+          Login with Facebook
+        </Button>
+      </div>
     </div>
   );
 }
